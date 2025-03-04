7f7af1bb0d434c7660611800c5b974ee8dad5d3d
NarayanPhukan <145931372+NarayanPhukan@users.noreply.github.com>
2025-03-04 11:29:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toggle = document.querySelector('.toggle');
-    var nav = document.querySelector('header nav');
-
-    toggle.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
-});
